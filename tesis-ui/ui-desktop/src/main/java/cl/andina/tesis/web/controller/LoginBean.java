5638df1cb71b4e49aea86b7a3444660ee220405c
NachoVergaraC <ignacio.vergarac@gmail.com>
2018-11-27 12:49:16 -0300

    Avances 27/11

---
+++ tesis-ui/ui-desktop/src/main/java/cl/andina/tesis/web/controller/LoginBean.java
@@ -0,0 +1,25 @@
+package cl.andina.tesis.web.controller;
+
+
+import java.io.Serializable;
+
+public class LoginBean
+        implements Serializable
+{
+    public String pepito;
+
+    public void init()
+    {
+        pepito = "Aca completo el contexto";
+    }
+
+    public String getPepito()
+    {
+        return pepito;
+    }
+
+    public void setPepito(String pepito)
+    {
+        this.pepito = pepito;
+    }
+}
